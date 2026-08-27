60002d137b17560acade8f6393a5c776c12bc0e0
vxyzview <vxyzview@gmail.com>
2026-08-27 21:59:48 +0000

    fix: SettingsTerminalScreen missing stringResource import (overwritten by NavController import)

--- features/terminal/src/main/java/com/rk/settings/terminal/SettingsTerminalScreen.kt
+++ features/terminal/src/main/java/com/rk/settings/terminal/SettingsTerminalScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.rk.DocumentProvider
 import com.rk.DefaultScope
