449280325aa5034622c24963f2f1012758dffb9d
vxyzview <vxyzview@gmail.com>
2026-08-31 00:05:15 +0700

    fix(ui): add explicit awaitFirstDown top-level import

--- core/main/src/main/java/com/rk/components/ResponsiveDrawer.kt
+++ core/main/src/main/java/com/rk/components/ResponsiveDrawer.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.input.pointer.AwaitPointerEventScope
+import androidx.compose.ui.input.pointer.awaitFirstDown
 import androidx.compose.ui.input.pointer.changedToUp
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
